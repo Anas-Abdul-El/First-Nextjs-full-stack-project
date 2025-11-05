97f8ba9ae35796e3a192db5a2c18dfd257da692d
Anas Abdul El <anasabdulel@gmail.com>
2025-11-05 19:22:58 +0200

    some

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
             },
         });
 
-        return NextResponse.json({ message: "User created", user: newUser }, { status: 201 });
+        return NextResponse.json({ message: "User created" }, { status: 201 });
     } catch (error) {
         console.error("Error creating user:", error);
         return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
