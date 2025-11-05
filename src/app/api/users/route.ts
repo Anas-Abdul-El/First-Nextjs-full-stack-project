080663137f12b5795db86b67316a357b9366df75
Anas Abdul El <anasabdulel@gmail.com>
2025-11-05 19:13:50 +0200

    some

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../../../lib/prisma"//ح نعمله بعد شوي
+import prisma from "../../../../lib/prisma"//ح نعمله بعد شوي
 
 export async function POST(req: Request) {
     try {
